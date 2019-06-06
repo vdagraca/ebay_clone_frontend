f8acbc2afc4aa690ec0b176fae847cc0ac36b2ce
Vivian da Graca <vivian.da.graca@gmail.com>
2019-06-06 21:27:27 +0200

    css

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,20 +1,28 @@
 import React from 'react'
+import './ProductDetails.css'
+
 
 export default function ProductDetails(props) {
 
     const { product } = props
     return (
         <div>
-            <li>Title:{product.title}</li>
-            <li>Description:{product.description}</li>
-            <br />
-            <img style={{ height: 400 }} src={product.picture} alt={product.title} />
-            <br /><br />
-            <li>Price:{product.price}</li>
-            <li>Email:{product.email}</li>
-            <li>Phonenumber:{product.phonenumber}</li>
-            <button onClick={props.goBack}>Go back</button>
-            <button onClick={props.onEdit}>Edit</button>
+            <h2>{product.title}</h2>
+            <div className="product-details">
+                <div>
+                    <img style={{ height: 400 }} src={product.picture} alt={product.title} />
+                    <br /><br />
+                    <p>Price:{product.price}</p>
+                    <p>Email:{product.email}</p>
+                    <p>Phonenumber:{product.phonenumber}</p>
+                    <button onClick={props.goBack}>Go back</button>
+                    <button onClick={props.onEdit}>Edit</button>
+                </div>
+                <div>
+                    <h3>Description:</h3>
+                    <p>{product.description}</p>
+                </div>
+            </div>
         </div>
     )
 }
