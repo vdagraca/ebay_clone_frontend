321c758a1d75d47db0d8bc6ac035a31afd508a29
Vivian da Graca <vivian.da.graca@gmail.com>
2019-06-11 12:55:48 +0200

    background header, buttons, details

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -7,20 +7,20 @@ export default function ProductDetails(props) {
     const { product } = props
     return (
         <div>
-            <h2>{product.title}</h2>
+            <h1>Product details</h1>
             <div className="product-details">
                 <div>
                     <img style={{ height: 400 }} src={product.picture} alt={product.title} />
                     <br /><br />
-                    <p>Price:{product.price}</p>
-                    <p>Email:{product.email}</p>
-                    <p>Phonenumber:{product.phonenumber}</p>
+                    <h2>{product.title}</h2>
+                    <p>{product.description}</p>
                     <button onClick={props.goBack}>Go back</button>
                     <button onClick={props.onEdit}>Edit</button>
                 </div>
                 <div>
-                    <h3>Description:</h3>
-                    <p>{product.description}</p>
+                    <p>€ {product.price}</p>
+                    <p>Email:{product.email}</p>
+                    <p>Phonenumber:{product.phonenumber}</p>
                 </div>
             </div>
         </div>
